841595764db7e362f5c5e31f9bcaa5cc87e7cb0d
lzk800 <lzk800@gmail.com>
2015-05-13 08:21:25 +0800

    modified:   bin/mini-harp.js

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var minimist = require('minimist');
-var createMiniHarp = require('../index');
+var createMiniHarp = require('../');
 var parseArgs = minimist(process.argv.slice(2));
 var port = parseArgs.port || 4000;
 var root = parseArgs._[0] || process.cwd();
