fe65c642e4e019e61f2ba0468731998f563a50bb
lzk800 <lzk800@gmail.com>
2015-05-13 08:15:17 +0800

    modified:   bin/mini-harp.js 	modified:   index.js 	new file:   lib/URLrewrite.js 	modified:   lib/processor/jade.js 	modified:   lib/processor/less.js 	new file:   lib/processor/prevent.js

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
-var miniHarp = require("mini-harp");
-var argv = require('minimist')(process.argv.slice(2));
-var port = argv['port'] ? argv['port'] : 4000;
-var path = argv._[0] ? argv._[0] : process.cwd(); // current directory
-
-app = miniHarp(path);
-console.log("Starting mini-harp on http://localhost:" + port);
-app.use(function(request,response,next) {
-	if(request.url == '/current-time'){
-		response.end(new Date().toISOString()+'\n');
-	}else{
-		next();
-	}
-}).listen(port);
+var minimist = require('minimist');
+var createMiniHarp = require('../index');
+var parseArgs = minimist(process.argv.slice(2));
+var port = parseArgs.port || 4000;
+var root = parseArgs._[0] || process.cwd();
+var app = createMiniHarp(root);
+app.listen(port);
+console.log("App start listening on port " + port);
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var miniharp = function (root) {
+	var connect = require('connect');
+	var serveStatic = require('serve-static');
+	var path = require('path');
+	var prevent = require('./lib/processor/prevent')
+	var makeJade = require('./lib/processor/jade');
+	var makeLess = require('./lib/processor/less');
+	var rewriteURL = require('./lib/URLrewrite');
+	var miniHarp = connect();
+	miniHarp.use(rewriteURL());
+	miniHarp.use(prevent(root));
+	miniHarp.use(serveStatic(root));
+	miniHarp.use(makeJade(root));
+	miniHarp.use(makeLess(root));
+	return miniHarp;
+};
 
-module.exports = function(path){
-    return connect().use(serveStatic(path));
-}
+module.exports = miniharp;
